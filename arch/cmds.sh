53a7414651f612eb6f39f8d7f7dac358dc33394a
Zeyad <64292515+zeyadhassan@users.noreply.github.com>
2026-08-31 15:34:42 +0300

    Update cmds.sh

--- arch/cmds.sh
+++ arch/cmds.sh
@@ -245,3 +245,101 @@ curl -fsS http://127.0.0.1:8899/v1/models | jq
 
 podman-compose -f "$AI_COMPOSE_FILE" up -d nginx
 
+____________________________________________________________
+
+'nim-qwen7b|nim-llama70b|nim-qwen3vl|nim-gptoss120b|depends_on:|8889|8890|8897|8899' \
+"$AI_COMPOSE_FILE"
+24:    depends_on:
+26:      - nim-gptoss120b
+27:      - nim-qwen3vl
+42:      NO_PROXY: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b"
+45:      no_proxy: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b"
+65:  nim-qwen7b:
+67:    container_name: nim-qwen7b
+70:      - "8889:8000"
+75:      NO_PROXY: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b"
+78:      no_proxy: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b"
+90:  nim-llama70b:
+92:    container_name: nim-llama70b
+95:      - "8890:8000"
+100:      NO_PROXY: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b"
+103:      no_proxy: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b"
+120:  nim-qwen3vl:
+122:    container_name: nim-qwen3vl
+129:      - "8899:8000"
+146:  nim-gptoss120b:
+148:    container_name: nim-gptoss120b
+151:      - "8897:8000"
+156:      NO_PROXY: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b,nim-gptoss120b"
+159:      no_proxy: "localhost,127.0.0.1,0.0.0.0,host.containers.internal,ollama1,nim-gemma,nim-qwen7b,nim-llama70b,nim-gptoss120b"
+[srv_mlengineering@cbq2-svd-dsgpu2 ~]$ grep -nC 2 -E \
+'backend_qwen|backend_llama|backend_gptoss|8889|8890|8897|8899' \
+"$AI_NGINX_FILE"
+22-   # }
+23-
+24:    upstream backend_qwen7b {
+25:        server 10.1.94.110:8889;
+26-        keepalive 32;
+27-    }
+28-
+29:    upstream backend_llama70b {
+30:        server 10.1.94.110:8890;
+31-        keepalive 32;
+32-    }
+33-
+34:    upstream backend_gptoss120b {
+35:        server 10.1.94.110:8897;
+36-        keepalive 32;
+37-    }
+38-
+39-
+40:    upstream backend_qwen3vl {
+41:        server 10.1.94.110:8899;
+42-        keepalive 32;
+43-    }
+--
+84-        location @_backend_ollama1 { proxy_pass http://backend_ollama1; }
+85-        # location @_backend_gemma { proxy_pass http://backend_gemma; }
+86:        location @_backend_qwen7b { proxy_pass http://backend_qwen7b; }
+87:        location @_backend_llama70b { proxy_pass http://backend_llama70b; }
+88:        location @_backend_qwen3vl { proxy_pass http://backend_qwen3vl; }
+89:        location @_backend_gptoss120b { proxy_pass http://backend_gptoss120b; }
+90-   }
+91-}
+[srv_mlengineering@cbq2-svd-dsgpu2 ~]$ grep -nC 2 -E \
+'qwen|llama|gpt-oss' \
+"$AI_ROUTER_FILE"
+1-function get_backend(r) {
+2-    if (r.method === 'GET') {
+3:        r.internalRedirect("@_backend_ollama1");
+4-        return;
+5-    }
+--
+17-    }
+18-
+19:    if (model.indexOf("qwen3-vl") !== -1) {
+20:        r.internalRedirect("@_backend_qwen3vl");
+21-        return;
+22-    }
+--
+27-    }
+28-
+29:    if (model.indexOf("qwen-2.5-7b-instruct") !== -1) {
+30:        r.internalRedirect("@_backend_qwen7b");
+31-        return;
+32-    }
+33-
+34:    if (model.indexOf("gpt-oss-120b") !== -1) {
+35-        r.internalRedirect("@_backend_gptoss120b");
+36-        return;
+37-    }
+38-
+39:    if (model.indexOf("llama-3.3-70b-instruct") !== -1) {
+40:        r.internalRedirect("@_backend_llama70b");
+41-        return;
+42-    }
+43-
+44:    // Default fallback to Ollama
+45:    r.internalRedirect("@_backend_ollama1");
+46-    return;
+47-}
